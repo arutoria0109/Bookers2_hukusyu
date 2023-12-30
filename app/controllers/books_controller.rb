57b7a7bc3fb97e4a0a7fbb7fb228875f5087c103
EC2 Default User <samanimaya017@gmail.com>
2023-12-30 15:59:00 +0900

    CREATE 追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,19 +3,22 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
-    @book = book.new(book_params)
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book.id)
   end
-  
-  
-  
-  private
-  
-  def book_params
-    params.require(:book).permit(:title, :body)
+
+  def show
+    @book = Book.find(params[:id])
   end
-  
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body,)
+  end
+
 end
